ee5db9b0882c4085194c581cb938bfe30752ad4c
xy <xy@frogdesign.com>
2017-08-02 16:15:55 -0400

    add word class

--- src/js/app.coffee
+++ src/js/app.coffee
@@ -22,33 +22,66 @@ isMenuDisplayed = false
 #   return
 
 ctx = null
+curr = null
+words = []
+
+class Word
+  constructor: (@ctx, @id, @text, @link, @x, @y) ->
+    @width = @ctx.measureText(@text).width
+  update: ->
+    if curr != null && @id == curr.id
+      @color = @ctx.createLinearGradient(@x, @y, @x + @width, @y)
+      stop = (mouseX - curr.x) / @width
+      console.log stop
+      @color.addColorStop("0", "#111")
+      @color.addColorStop(stop, "#fff")
+      @color.addColorStop("1.0", "#111")
+    else
+      @color = @ctx.createLinearGradient(@x, @y, @x + @width, @y)
+      @color.addColorStop("0", "#111")
+      @color.addColorStop("1.0", "#fff")
+  draw: ->
+    @ctx.fillStyle = @color
+    @ctx.fillText(@text, @x, @y)
 
 setup = ->
   canvas = createCanvas(windowWidth, windowHeight)
   ctx = canvas.drawingContext
   canvas.id('canvas').position(0, 0).style('position', 'absolute')
-  background(0)
   stories = selectAll('.story-item');
   ctx.font = "27px San Francisco";
 
   startX = 0
-  startY = 25
+  startY = 50
   index = 0
   while startY < windowHeight
     story = stories[index]
     link = story.elt.href
     text = story.elt.innerHTML
-    textWidth = ctx.measureText(text).width;
-    gradient = ctx.createLinearGradient(startX, startY, startX + textWidth, startY);
-    gradient.addColorStop("0", "#111");
-    gradient.addColorStop("1.0", "#fff");
-    ctx.fillStyle = gradient;
-    ctx.fillText(text, startX, startY);
-    startX += textWidth + 20
+    word = new Word ctx, index, text, link, startX, startY
+    words.push word
+    spacing = random 10, 40
+    startX += word.width + spacing
     if startX > windowWidth
-      startX = windowWidth - startX
+      startX = startX - spacing - windowWidth - word.width
       startY += 50
     else
       index = (index + 1) % stories.length
 
 draw = ->
+  background 0
+
+  # find mouseover
+  curr = null
+  for word in words
+    if mouseX > word.x && mouseX < word.x + word.width && mouseY < (word.y + 10) && mouseY > (word.y - 30)
+      curr = word
+      break
+
+  for word in words
+    word.update()
+    word.draw()
+
+mouseClicked = ->
+  if curr != null
+    window.location = curr.link
--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,70 @@
-var ctx, draw, isMenuDisplayed, setup;
+var Word, ctx, curr, draw, isMenuDisplayed, mouseClicked, setup, words;
 
 isMenuDisplayed = false;
 
 ctx = null;
 
+curr = null;
+
+words = [];
+
+Word = (function() {
+  function Word(ctx1, id, text1, link1, x, y) {
+    this.ctx = ctx1;
+    this.id = id;
+    this.text = text1;
+    this.link = link1;
+    this.x = x;
+    this.y = y;
+    this.width = this.ctx.measureText(this.text).width;
+  }
+
+  Word.prototype.update = function() {
+    var stop;
+    if (curr !== null && this.id === curr.id) {
+      this.color = this.ctx.createLinearGradient(this.x, this.y, this.x + this.width, this.y);
+      stop = (mouseX - curr.x) / this.width;
+      console.log(stop);
+      this.color.addColorStop("0", "#111");
+      this.color.addColorStop(stop, "#fff");
+      return this.color.addColorStop("1.0", "#111");
+    } else {
+      this.color = this.ctx.createLinearGradient(this.x, this.y, this.x + this.width, this.y);
+      this.color.addColorStop("0", "#111");
+      return this.color.addColorStop("1.0", "#fff");
+    }
+  };
+
+  Word.prototype.draw = function() {
+    this.ctx.fillStyle = this.color;
+    return this.ctx.fillText(this.text, this.x, this.y);
+  };
+
+  return Word;
+
+})();
+
 setup = function() {
-  var canvas, gradient, index, link, results, startX, startY, stories, story, text, textWidth;
+  var canvas, index, link, results, spacing, startX, startY, stories, story, text, word;
   canvas = createCanvas(windowWidth, windowHeight);
   ctx = canvas.drawingContext;
   canvas.id('canvas').position(0, 0).style('position', 'absolute');
-  background(0);
   stories = selectAll('.story-item');
   ctx.font = "27px San Francisco";
   startX = 0;
-  startY = 25;
+  startY = 50;
   index = 0;
   results = [];
   while (startY < windowHeight) {
     story = stories[index];
     link = story.elt.href;
     text = story.elt.innerHTML;
-    textWidth = ctx.measureText(text).width;
-    gradient = ctx.createLinearGradient(startX, startY, startX + textWidth, startY);
-    gradient.addColorStop("0", "#111");
-    gradient.addColorStop("1.0", "#fff");
-    ctx.fillStyle = gradient;
-    ctx.fillText(text, startX, startY);
-    startX += textWidth + 20;
+    word = new Word(ctx, index, text, link, startX, startY);
+    words.push(word);
+    spacing = random(10, 40);
+    startX += word.width + spacing;
     if (startX > windowWidth) {
-      startX = windowWidth - startX;
+      startX = startX - spacing - windowWidth - word.width;
       results.push(startY += 50);
     } else {
       results.push(index = (index + 1) % stories.length);
@@ -37,4 +73,28 @@ setup = function() {
   return results;
 };
 
-draw = function() {};
+draw = function() {
+  var i, j, len, len1, results, word;
+  background(0);
+  curr = null;
+  for (i = 0, len = words.length; i < len; i++) {
+    word = words[i];
+    if (mouseX > word.x && mouseX < word.x + word.width && mouseY < (word.y + 10) && mouseY > (word.y - 30)) {
+      curr = word;
+      break;
+    }
+  }
+  results = [];
+  for (j = 0, len1 = words.length; j < len1; j++) {
+    word = words[j];
+    word.update();
+    results.push(word.draw());
+  }
+  return results;
+};
+
+mouseClicked = function() {
+  if (curr !== null) {
+    return window.location = curr.link;
+  }
+};
